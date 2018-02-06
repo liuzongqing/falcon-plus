2402c3c2888beb746036fd2b7c0b77d0a68e74d0
zongqing.liu <zongqing.liu@funplus.com>
2018-02-06 14:41:55 +0800

    适应于duty修改

--- modules/judge/store/judge.go
+++ modules/judge/store/judge.go
@@ -90,7 +90,9 @@ func sendEvent(event *model.Event) {
 	}
 
 	// send to redis
-	redisKey := fmt.Sprintf(g.Config().Alarm.QueuePattern, event.Priority())
+	// redisKey := fmt.Sprintf(g.Config().Alarm.QueuePattern, event.Priority())
+	// kingsgroup修改，只向一个队列里写报警事件: event:p0
+	redisKey := fmt.Sprintf(g.Config().Alarm.QueuePattern, 0)
 	rc := g.RedisConnPool.Get()
 	defer rc.Close()
 	rc.Do("LPUSH", redisKey, string(bs))
@@ -211,19 +213,21 @@ func sendEventIfNeed(historyData []*model.HistoryData, isTriggered bool, now int
 			event.CurrentStep = 1
 
 			// 但是有些用户把最大报警次数配置成了0，相当于屏蔽了，要检查一下
-			if maxStep == 0 {
-				return
-			}
+			// kingsgroup修改，取消最大报警次数的限制
+			// if maxStep == 0 {
+			// 	return
+			// }
 
 			sendEvent(event)
 			return
 		}
 
 		// 逻辑走到这里，说明之前Event是PROBLEM状态
-		if lastEvent.CurrentStep >= maxStep {
-			// 报警次数已经足够多，到达了最多报警次数了，不再报警
-			return
-		}
+		// kingsgroup修改，取消最大报警次数的限制
+		// if lastEvent.CurrentStep >= maxStep {
+		// 	// 报警次数已经足够多，到达了最多报警次数了，不再报警
+		// 	return
+		// }
 
 		if historyData[len(historyData)-1].Timestamp <= lastEvent.EventTime {
 			// 产生过报警的点，就不能再使用来判断了，否则容易出现一分钟报一次的情况
@@ -231,10 +235,11 @@ func sendEventIfNeed(historyData []*model.HistoryData, isTriggered bool, now int
 			return
 		}
 
-		if now-lastEvent.EventTime < g.Config().Alarm.MinInterval {
-			// 报警不能太频繁，两次报警之间至少要间隔MinInterval秒，否则就不能报警
-			return
-		}
+		// kingsgroup修改, 报警间隔由duty控制，取消这里的限制
+		// if now-lastEvent.EventTime < g.Config().Alarm.MinInterval {
+		// 	// 报警不能太频繁，两次报警之间至少要间隔MinInterval秒，否则就不能报警
+		// 	return
+		// }
 
 		event.CurrentStep = lastEvent.CurrentStep + 1
 		sendEvent(event)
